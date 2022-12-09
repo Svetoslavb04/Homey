174a5bd2dc6d3806f635205a5584ccecc90cf1b3
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-12-09 19:54:56 +0200

    Create Initial Theme

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
+import Button from '@mui/material/Button';
+import MaterialUIProvider from '../MaterialUI/MaterialUIProvider';
 
 function App() {
   return (
     <div className="App">
-      
+      <MaterialUIProvider>
+        <Button variant="contained">Hello World</Button>
+      </MaterialUIProvider>
     </div>
   );
 }

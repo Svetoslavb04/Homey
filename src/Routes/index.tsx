cc6409020b4f91ae86ac1c22b1bbda050e080b9b
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-12-14 21:03:10 +0200

    Remove misc

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,10 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from '../Pages/Home';
 
 const RoutesList = () => {
     return (
         <Routes>
-            <Route index path='/' element={<Home />} />
         </Routes>
     )
 }

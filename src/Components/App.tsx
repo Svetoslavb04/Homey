31df083a1b4cbadc68a0657076a67b34250a2863
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-12-14 19:39:10 +0200

    Added App Layout

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 
 import MaterialUIProvider from './Core/MaterialUI'; //Everything provided by MUI
+import Layout from './Core/Layout'; //Page layout
 
 function App() {
   return (
     <div className="App">
       <MaterialUIProvider>
         <BrowserRouter>
+          <Layout />
         </BrowserRouter>
       </MaterialUIProvider>
     </div>

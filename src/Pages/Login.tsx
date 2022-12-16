ed2eee223243eef6c0568fb1a9fb7a43b3bf5b74
martinyovkov <martiyovkov10@gmail.com>
2022-12-16 19:56:14 +0200

    improve login form

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,15 +1,67 @@
 import './Login.scss';
 import React from 'react';
-import {FC} from 'react';
+import {FC, useState} from 'react';
 import UserIcon from '@mui/icons-material/AccountCircleOutlined';
+import TextField from '@mui/material/TextField';
+
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import FilledInput from '@mui/material/FilledInput';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const Login:FC= () => {
+
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
+
   return (
     <div className='login-container'>
         <UserIcon className='user-icon' fontSize="large"/>
-         <h1>Login</h1> 
+
+        <div className='login-form'>
+            <TextField className='username'
+            error={false}
+            id="username"
+            label="Username"
+            type="username"
+            variant="standard"
+            />
+
+            <FormControl sx={{ m: 2, width: '200px' }} variant="standard">
+            <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+            <Input
+                id="standard-adornment-password"
+                type={showPassword ? 'text' : 'password'}
+                endAdornment={
+                <InputAdornment position="end">
+                    <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                </InputAdornment>
+                }
+            />
+            </FormControl>
+        </div>
     </div>
   )
 }
+
+
 
 export default Login;

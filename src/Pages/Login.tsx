ebf8d4e232ee51c54524a2c61ff33e5fe78852f0
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-12-19 17:45:33 +0200

    Updated Login page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,64 +1,64 @@
 import './Login.scss';
-import React from 'react';
-import {FC, useState} from 'react';
+import { FC, useState } from 'react';
+
 import UserIcon from '@mui/icons-material/AccountCircleOutlined';
-import TextField from '@mui/material/TextField';
-import IconButton from '@mui/material/IconButton';
-import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormControl from '@mui/material/FormControl';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
-const Login:FC= () => {
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
 
-    const [showPassword, setShowPassword] = React.useState(false);
+const Login: FC = () => {
+
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleClickShowPassword = () => setShowPassword((show) => !show);
 
-    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-        event.preventDefault();
-    };
-
-  return (
-    <div className='login-container'>
-        <UserIcon className='user-icon' fontSize="large"/>
-
-        <form className='login-form'>
-
-            <TextField className='username'
-            error={false}
-            id="username"
-            label="Username"
-            type="username"
-            variant="standard"
-            />
-
-            <FormControl sx={{ m: 2, width: '200px' }} variant="standard">
-            <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-            <Input
-                id="standard-adornment-password"
-                type={showPassword ? 'text' : 'password'}
-                endAdornment={
-                <InputAdornment position="end">
-                    <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                    >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                </InputAdornment>
-                }
-            />
-            </FormControl>
-            
-            <button id='login-button' type='submit'> Login </button>
-
-        </form>
-    </div>
-  )
+    return (
+        <div id='login-container'>
+            <div id="login-user-logo-container">
+                <UserIcon className='user-icon' fontSize="large" />
+            </div>
+            <form id='login-form'>
+                <div id="login-username">
+                    <TextField
+                        fullWidth
+                        label="Username"
+                        variant="standard"
+                        color="secondary"
+                        error={false}
+                    />
+                </div>
+                <div id="login-password">
+                    <FormControl fullWidth variant="standard" color="secondary">
+                        <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                        <Input
+                            id="standard-adornment-password"
+                            type={showPassword ? 'text' : 'password'}
+                            endAdornment={
+                                <InputAdornment position="end" className='password-visibilty-icon'>
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                    >
+                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        />
+                    </FormControl>
+                </div>
+                <div id='login-button-container'>
+                    <Button fullWidth variant="contained" size='large' id='login-button' type='submit'>Login</Button>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 

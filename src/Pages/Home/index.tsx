672489b9be7e5e305e5fa334efb3821b7b94c42f
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-12-28 12:51:48 +0200

    Finished top properties listing

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import './Home.scss';
 import { FC, useRef } from 'react'
 
+import { useNavigate } from "react-router-dom";
+
 import { Splide, SplideSlide } from '@splidejs/react-splide'
 import '@splidejs/react-splide/css'
 
@@ -12,6 +14,8 @@ import VillaIcon from '@mui/icons-material/Villa';
 import LandscapeIcon from '@mui/icons-material/Landscape';
 
 const Home: FC = () => {
+
+    const navigate = useNavigate();
 
     const slides: SlideshowPropertySlide[] = [
         {
@@ -42,6 +46,65 @@ const Home: FC = () => {
     ]
 
     const splideRef = useRef<Splide>(null);
+
+    //Fetch top properties
+    const topProperties = [
+        {
+            id: '1',
+            image: {
+                src: 'assets/images/modern_house_1.jpg',
+                alt: 'house'
+            },
+            town: 'Sofia',
+            name: 'Modern House'
+        }, {
+            id: '2',
+            image: {
+                src: 'assets/images/modern_house_2.jpg',
+                alt: 'house'
+            },
+            town: 'Sofia',
+            name: 'Modern House'
+        }, {
+            id: '3',
+            image: {
+                src: 'assets/images/modern_house_3.jpg',
+                alt: 'house'
+            },
+            town: 'Sofia',
+            name: 'Modern House'
+        }, {
+            id: '4',
+            image: {
+                src: 'assets/images/modern_house_4.jpg',
+                alt: 'house'
+            },
+            town: 'Sofia',
+            name: 'Modern House'
+        }, {
+            id: '5',
+            image: {
+                src: 'assets/images/modern_house_7.jpg',
+                alt: 'house'
+            },
+            town: 'Sofia',
+            name: 'Modern House'
+        }, {
+            id: '6',
+            image: {
+                src: 'assets/images/modern_house_8.jpg',
+                alt: 'house'
+            },
+            town: 'Sofia',
+            name: 'Modern House'
+        }
+    ]
+
+    const handleTopPropertyClick = (id: string) => {
+
+        navigate(`/properties/${id}`);
+
+    }
 
     return (
         <div id='home-container'>
@@ -86,6 +149,49 @@ const Home: FC = () => {
                 <div className='home-property-card'>
                     <LandscapeIcon />
                     <h5>Landfields</h5>
+                </div>
+            </div>
+            <div id="home-top-properties">
+                <h2>Our top picks</h2>
+                <div id='home-top-properties-container'>
+                    <div>
+                        {
+                            topProperties.slice(0, 3)
+                                .map(prop =>
+                                    <div className='home-top-property' key={`${prop.name + prop.town + prop.image.src}`}>
+                                        <div
+                                            onClick={handleTopPropertyClick.bind(null, prop.id)}
+                                            className='home-top-property-image-wrapper'
+                                        >
+                                            <img src={prop.image.src} alt={prop.image.alt} />
+                                        </div>
+                                        <div className='home-top-property-description'>
+                                            <h5>{prop.town}</h5>
+                                            <p>{prop.name}</p>
+                                        </div>
+                                    </div>
+                                )
+                        }
+                    </div>
+                    <div>
+                        {
+                            topProperties.slice(3)
+                                .map(prop =>
+                                    <div className='home-top-property' key={`${prop.name + prop.town + prop.image.src}`}>
+                                        <div
+                                            onClick={handleTopPropertyClick.bind(null, prop.id)}
+                                            className='home-top-property-image-wrapper'
+                                        >
+                                            <img src={prop.image.src} alt={prop.image.alt} />
+                                        </div>
+                                        <div className='home-top-property-description'>
+                                            <h5>{prop.town}</h5>
+                                            <p>{prop.name}</p>
+                                        </div>
+                                    </div>
+                                )
+                        }
+                    </div>
                 </div>
             </div>
         </div >

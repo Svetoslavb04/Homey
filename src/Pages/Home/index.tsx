3da22a1ece717fe3a74636b2fec7ef310d2d2b20
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-01-21 14:00:11 +0200

    refactor: extract home slideshow in a component

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,73 +1,17 @@
 import './Home.scss';
-import { FC, useRef } from 'react'
+import { FC } from 'react'
 
 import { useNavigate } from "react-router-dom";
-import { usePubSupContext } from '../../contexts/PubSubContext';
-import { customEvents, Subscriber } from '../../utils/pubsub';
-
-import { Splide, SplideSlide } from '@splidejs/react-splide'
-import '@splidejs/react-splide/css'
-
-import SlideshowPropertySlide from '../../interfaces/SlideShowSlide'
 
 import HouseIcon from '@mui/icons-material/House';
 import ApartmentIcon from '@mui/icons-material/Apartment';
 import VillaIcon from '@mui/icons-material/Villa';
 import LandscapeIcon from '@mui/icons-material/Landscape';
-import useEffectSkipOnMount from '../../hooks/useEffectSkipOnMount';
+import HomeSlideShow from './Components/HomeSlideShow';
 
 const Home: FC = () => {
 
-    const eventBus = usePubSupContext();
     const navigate = useNavigate();
-
-    const splideRef = useRef<Splide>(null);
-
-    useEffectSkipOnMount(() => {
-        
-        const subscriber: Subscriber = {
-            event: customEvents.sidebarStateChange,
-            callback: () => {
-
-                setTimeout(() => {
-                    splideRef.current?.go('>');
-                    splideRef.current?.go('<');
-                }, 500)
-
-            }
-        }
-
-        eventBus?.subscribe(subscriber);
-
-    }, [eventBus])
-
-    const slides: SlideshowPropertySlide[] = [
-        {
-            image: {
-                src: 'assets/images/modern_house_1.jpg',
-                alt: 'house'
-            },
-            propertyType: 'Vacation',
-            header: 'Many beatuiful sea views',
-            description: 'Advice from the ocean: Be shore of yourself. Come out of your shell. Take time to coast. Sea life’s beauty. Don’t get so tide down on work that you miss out on life’s beautiful waves.'
-        }, {
-            image: {
-                src: 'assets/images/modern_house_6.jpg',
-                alt: 'house'
-            },
-            propertyType: 'After busy week',
-            header: 'Hidden among the trees',
-            description: 'The forest is not a resource for us, it is life itself. It is the only place for us to live.'
-        }, {
-            image: {
-                src: 'assets/images/modern_house_8.jpg',
-                alt: 'house'
-            },
-            propertyType: 'Everything at hand',
-            header: 'In the heart of the city',
-            description: 'No city should be too large for a man to walk out of in a morning.'
-        }
-    ]
 
     //Fetch top properties
     const topProperties = [
@@ -130,31 +74,7 @@ const Home: FC = () => {
 
     return (
         <div id='home-container'>
-            <div>
-                <Splide
-                    ref={splideRef}
-                    options={{
-                        rewindByDrag: true,
-                        type: 'loop',
-                        autoplay: false,
-                        interval: 3000,
-                        pauseOnHover: true,
-                        arrows: false,
-                        height: '100vh',
-                    }}>
-                    {slides.map(s =>
-                        <SplideSlide key={s.image.src}>
-                            <div className='home-slideshow-slide' style={{ backgroundImage: `url(${s.image.src})` }}>
-                                <div>
-                                    <h5 className='home-slide-property-type'>{s.propertyType}</h5>
-                                    <h2 className='home-slide-header'>{s.header}</h2>
-                                    <p className='home-slide-description'>{s.description}</p>
-                                </div>
-                            </div>
-                        </SplideSlide>
-                    )}
-                </Splide>
-            </div>
+            <HomeSlideShow />
             <div id='home-property-cards'>
                 <div className='home-property-card'>
                     <HouseIcon />

692d93c136e01e4a06b7b1b3d69eaddc1f09eed6
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-01-25 16:22:42 +0200

    feat: add initial property listing page

---
+++ src/Pages/Property/Listing/index.tsx
@@ -0,0 +1,11 @@
+import './PropertiesListings.scss';
+
+const PropertiesListings = () => {
+    return (
+        <div className='property-listing-container'>
+            
+        </div>
+    )
+}
+
+export default PropertiesListings

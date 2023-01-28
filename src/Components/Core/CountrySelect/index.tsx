d5aa7a630cf537f0eb98690ead30ffd2fa288fce
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-01-28 16:25:21 +0200

    feat: add CountrySelect component

---
+++ src/Components/Core/CountrySelect/index.tsx
@@ -0,0 +1,53 @@
+import './CountrySelect.scss';
+import { FC } from 'react';
+
+import { countries } from '../../../assets/js/countries';
+
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+
+type CountrySelectProps = {
+    className?: string,
+    label: string,
+    value: string,
+    onChange: (e: SelectChangeEvent<string>) => void
+}
+
+const CountrySelect: FC<CountrySelectProps> = ({ className, label, value, onChange }) => {
+    return (
+        <FormControl variant='standard' className={`country-select${className ? ` ${className}` : ''}`}>
+            <InputLabel id="country-select-label">{label}</InputLabel>
+            <Select
+                label={label}
+                placeholder={label}
+                MenuProps={{ classes: { paper: 'country-select-countries-list' } }}
+                value={value}
+                onChange={onChange}
+            >
+                {
+                    countries?.map(country =>
+                        <MenuItem
+                            key={country.label}
+                            value={country.code}
+                        >
+                            <div className='country-select-item'>
+                                <span className='country-select-flag-container'>
+                                    <img
+                                        loading="lazy"
+                                        src={`https://flagcdn.com/w320/${country.code.toLowerCase()}.png`}
+                                        alt="flag"
+                                    />
+                                </span>
+                                <span className='country-select-name'>{country.label}</span>
+                            </div>
+                        </MenuItem>
+                    )
+                }
+            </Select>
+        </FormControl>
+    )
+}
+
+export default CountrySelect

a2ae2485615245f2c3cde59b47a4b163c3b879f8
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-02-05 12:27:19 +0200

    feat: add property filter ui

--- src/Components/Core/PropertyFilter/index.tsx
+++ src/Components/Core/PropertyFilter/index.tsx
@@ -1,5 +1,5 @@
 import './PropertyFilter.scss';
-import { FC, useState, useEffect, HTMLAttributes, DetailedHTMLProps } from 'react'
+import { FC, useState, useEffect, HTMLAttributes, DetailedHTMLProps, ChangeEvent } from 'react'
 
 import CountrySelect from '../CountrySelect';
 
@@ -11,6 +11,14 @@ import MenuItem from '@mui/material/MenuItem';
 import Button from '@mui/material/Button';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import Slider from '@mui/material/Slider';
+import Collapse from '@mui/material/Collapse';
+
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import FormLabel from '@mui/material/FormLabel';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import RadioGroup from '@mui/material/RadioGroup';
+import Radio from '@mui/material/Radio';
+import { PropertyFilterActionType, PropertyFilterState, usePropertyFilter } from './usePropertyFilter';
 
 export type PropertyFilterProps = {
 
@@ -18,33 +26,36 @@ export type PropertyFilterProps = {
 
 const PropertyFilter: FC<PropertyFilterProps> = ({ className, ...rest }) => {
 
-  const [selectedCountry, setSelectedCountry] = useState<string>('BG');
-  const handleCountryChange = (e: SelectChangeEvent<string>) => setSelectedCountry(e.target.value)
-
-  const [selectedType, setSelectedType] = useState<PropertyType | 'Any'>('Any');
-  const handleTypeChange = (e: SelectChangeEvent<string>) => {
-
-    const value = e.target.value
-
-    if (value in PropertyType || value === 'Any') {
-      setSelectedType(value as PropertyType | 'Any')
-    }
-
+  const initialPropertyFilter: PropertyFilterState = {
+    country: 'BG',
+    type: 'Any',
+    priceRange: [0, 100000],
+    status: 'Any'
   }
 
-  const [priceRange, setPriceRange] = useState<number[]>([0, 100000]);
+  const [state, dispatch] = usePropertyFilter(initialPropertyFilter);
 
-  const handleChange = (e: Event, newValue: number | number[]) => {
-    setPriceRange(newValue as number[]);
-  };
+  const [advancedFilterOpened, setAdvancedFilterOpened] = useState<boolean>(false);
+
+  const handleCountryChange = (e: SelectChangeEvent<string>) =>
+    dispatch({ type: PropertyFilterActionType.SET_COUNTRY, payload: e.target.value })
+
+  const handleTypeChange = (e: SelectChangeEvent<string>) =>
+    dispatch({ type: PropertyFilterActionType.SET_TYPE, payload: e.target.value })
+
+  const handlePriceChange = (e: Event, newValue: number | number[]) =>
+    dispatch({ type: PropertyFilterActionType.SET_PRICE_RANGE, payload: newValue })
+
+  const handleStatusChange = (e: ChangeEvent<HTMLInputElement>) =>
+    dispatch({ type: PropertyFilterActionType.SET_STATUS, payload: e.target.value })
 
   useEffect(() => {
 
     fetch('http://ip-api.com/json')
       .then(res => res.json())
-      .then(payload => { setSelectedCountry(payload.countryCode) })
+      .then(payload => { dispatch({ type: PropertyFilterActionType.SET_COUNTRY, payload: payload.countryCode }) })
 
-  }, [])
+  }, [dispatch])
 
   return (
     <div {...rest} className={`property-filter${className ? ` ${className}` : ''}`}>
@@ -53,7 +64,7 @@ const PropertyFilter: FC<PropertyFilterProps> = ({ className, ...rest }) => {
           <CountrySelect
             className='property-filter-country-select'
             label='Country'
-            value={selectedCountry}
+            value={state.country}
             onChange={handleCountryChange}
           />
         </div>
@@ -61,7 +72,7 @@ const PropertyFilter: FC<PropertyFilterProps> = ({ className, ...rest }) => {
           <FormControl variant='standard' fullWidth>
             <InputLabel id="demo-simple-select-label">Type</InputLabel>
             <Select
-              value={selectedType}
+              value={state.type}
               label="Type"
               onChange={handleTypeChange}
             >
@@ -77,12 +88,12 @@ const PropertyFilter: FC<PropertyFilterProps> = ({ className, ...rest }) => {
           </FormControl>
         </div>
         <div className='property-filter-basic-item'>
-          <p className='property-filter-price-label'>Price: {priceRange[0]} - {priceRange[1]}</p>
+          <p className='property-filter-price-label'>Price: {state.priceRange[0]} - {state.priceRange[1]}</p>
           <Slider
             size="small"
             getAriaLabel={() => 'Price range'}
-            value={priceRange}
-            onChange={handleChange}
+            value={state.priceRange}
+            onChange={handlePriceChange}
             valueLabelDisplay="auto"
             min={0} //Should get from server
             max={100000} //Should get from server
@@ -90,6 +101,64 @@ const PropertyFilter: FC<PropertyFilterProps> = ({ className, ...rest }) => {
         </div>
         <div className='property-filter-basic-item'>
           <Button fullWidth variant="contained">Search Property</Button>
+        </div>
+      </div>
+      <div className='property-filter-advanced'>
+        <Collapse in={advancedFilterOpened}>
+          <div className='property-filter-advanced-content'>
+            <div className='property-filter-advanced-row'>
+              <div className='property-filter-advanced-item'>
+                <FormControl>
+                  <FormLabel className="property-filter-advanced-label">Status</FormLabel>
+                  <RadioGroup
+                    row
+                    name="status-radio-group"
+                    value={state.status}
+                    onChange={handleStatusChange}
+                  >
+                    <FormControlLabel
+                      value="for_sale"
+                      className='property-filter-advanced-value'
+                      control={
+                        <Radio
+                          disableRipple
+                          sx={{
+                            '& .MuiSvgIcon-root': {
+                              fontSize: 12,
+                              marginRight: 0
+                            },
+                          }} />
+                      }
+                      label="for sale"
+                    />
+                    <FormControlLabel
+                      value="for_rent"
+                      className='property-filter-advanced-value'
+                      control={
+                        <Radio
+                          disableRipple
+                          sx={{
+                            '& .MuiSvgIcon-root': {
+                              fontSize: 12,
+                              marginRight: 0
+                            },
+                          }} />
+                      }
+                      label="for rent"
+                    />
+                  </RadioGroup>
+                </FormControl>
+              </div>
+              <div className='property-filter-advanced-item'>
+              </div>
+            </div>
+          </div>
+        </Collapse>
+        <div className='property-filter-advanced-toggler'>
+          <div onClick={() => setAdvancedFilterOpened(o => !o)}>
+            <p>Advanced Search</p>
+            <KeyboardArrowDownIcon fontSize='small' />
+          </div>
         </div>
       </div>
     </div>

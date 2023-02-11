be7cdd31787be6178ba55d8a57cd3db07199f79c
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-02-12 00:43:15 +0200

    feat: add static property listing page

---
+++ src/Pages/Property/Listing/Components/PropertiesSection/index.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+import PropertyCardExtended from '../../../../../Components/Core/PropertyCardExtended';
+import { IProperty } from '../../../../../interfaces/IProperty';
+
+import './PropertiesSection.scss';
+
+type Props = {
+    properties: IProperty[]
+}
+
+const PropertiesSection: FC<Props> = ({ properties }) => {
+
+    return (
+        <section className='properties-listing-section'>
+            <div>
+                {
+                    properties.filter((p, i) => i % 2 === 0).map(p => <PropertyCardExtended key={p._id} property={p} />)
+                }
+            </div>
+            <div>
+                {
+                    properties.filter((p, i) => i % 2 !== 0).map(p => <PropertyCardExtended key={p._id} property={p} />)
+                }
+            </div>
+        </section>
+    )
+}
+
+export default PropertiesSection
--- src/Pages/Property/Listing/index.tsx
+++ src/Pages/Property/Listing/index.tsx
@@ -1,16 +1,23 @@
 import { useState } from 'react';
+import { PropertyStatus } from '../../../enums/PropertyStatus';
+import { PropertyType } from '../../../enums/PropertyType';
+import { IProperty } from '../../../interfaces/IProperty';
 
 import IPropertyFilter from '../../../interfaces/IPropertyFilter';
 import FilterSection from './Components/FilterSection';
+import PropertiesSection from './Components/PropertiesSection';
 import './PropertiesListings.scss';
 
 const PropertiesListings = () => {
 
-    const [filter, setFilter] = useState<IPropertyFilter | null>(null)
-    
+    const [filter, setFilter] = useState<IPropertyFilter | null>(null);
+
+    const [properties, setProperties] = useState<IProperty[]>([]);
+
     return (
         <div className='property-listing-container'>
             <FilterSection setFilter={setFilter} />
+            <PropertiesSection properties={properties} />
         </div>
     )
 }

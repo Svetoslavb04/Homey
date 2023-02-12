b016a45d5c5760f6a15dea6e954710b4a9c3e736
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-02-12 03:21:53 +0200

    feat: add Homey API endpoints

--- src/assets/js/APIs.ts
+++ src/assets/js/APIs.ts
@@ -1,2 +1,11 @@
 export const countriesnowBaseURL = 'https://countriesnow.space/api/v0.1'
 export const citiesOfCountryURL = `${countriesnowBaseURL}/countries/cities`
+
+export const homeyAPIBaseURL = 'http://localhost:3001'
+export const homeyAPI = {
+    me: `${homeyAPIBaseURL}/me`,
+    login: `${homeyAPIBaseURL}/login`,
+    logout: `${homeyAPIBaseURL}/logout`,
+    registerUser: `${homeyAPIBaseURL}/register/user`,
+    registerAgency: `${homeyAPIBaseURL}/register/agency`,
+}

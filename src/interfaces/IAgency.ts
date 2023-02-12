601d1300d7881fc07dde4c3a947d8be4756905eb
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-02-12 03:24:20 +0200

    feat: add IAgency & IUser intefaces

---
+++ src/interfaces/IAgency.ts
@@ -0,0 +1,8 @@
+export interface IAgency {
+    _id: string,
+    email: string,
+    agencyName: string,
+    city: string,
+    address: string,
+    phoneNumber?: string,
+}
---
+++ src/interfaces/IUser.ts
@@ -0,0 +1,9 @@
+export interface IUser {
+    _id: string,
+    email: string,
+    firstName: string,
+    lastName: string,
+    phoneNumber?: string,
+    city?: string,
+    address?: string,
+}

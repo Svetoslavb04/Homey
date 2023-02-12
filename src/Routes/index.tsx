d78ef77434879e2d548d3389a2377362b04ba5d5
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-02-12 14:03:59 +0200

    feat: add route protection

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,16 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
+import Authenticated from './Authenticated';
 
 import Login from '../Pages/Auth/Login';
-import Logout from '../Pages/Auth/Logout';
 import Register from '../Pages/Auth/Register';
 import Home from '../Pages/Home';
 import PropertiesListing from '../Pages/Property/Listing';
+import Logout from '../Pages/Auth/Logout';
 
 const RoutesList = () => {
     return (
         <Routes>
             <Route element={<Login />} path='/login' />
-            <Route element={<Logout />} path='/logout' />
+            <Route element={<Authenticated><Logout /></Authenticated>} path='/logout' />
             <Route element={<Register />} path='/register' />
             <Route index path='/' element={<Home />} />
             <Route index path='/properties' element={<PropertiesListing />} />

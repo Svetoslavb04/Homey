e20714b03f9170437183514539c9ea9543835ea1
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-02-12 12:41:09 +0200

    feat: add register form validation

--- src/Pages/Auth/Register.tsx
+++ src/Pages/Auth/Register.tsx
@@ -1,6 +1,9 @@
 import './Auth.scss';
-import { FC, useState } from 'react';
+import { FC, FormEvent, useState } from 'react';
+
 import { NavLink } from "react-router-dom";
+
+import { ActionTypes, FormDataMode, useRegisterFormData } from './useRegisterFormData';
 
 import PersonIcon from '@mui/icons-material/Person';
 import GroupsIcon from '@mui/icons-material/Groups';
@@ -9,14 +12,8 @@ import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
-import Input from '@mui/material/Input';
 import Button from '@mui/material/Button';
-import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
-import FormControl from '@mui/material/FormControl';
-
-
-
 
 const Register: FC = () => {
 
@@ -26,64 +23,57 @@ const Register: FC = () => {
     const handleClickShowPassword = () => setShowPassword((show) => !show);
     const handleClickShowRePassword = () => setShowRePassword((show) => !show);
 
-    const [registerTypeIsUser, setUserType] = useState(true);
+    const [formData, dispatch] = useRegisterFormData();
 
-    const changeRegisterTypeToAgency = () => {
-        setUserType(false);
+    const handleFormSubmit = (e: FormEvent) => {
+        e.preventDefault();
     }
-
-    const changeRegisterTypeToUser = () => {
-        setUserType(true);
-    }
-
 
     return (
 
         <div id='image-background'>
             <div id='auth-container'>
                 <div className='auth-form-wrapper auth-register-form-wrapper'>
-                    <form id='auth-form'>
+                    <form id='auth-form' onSubmit={handleFormSubmit}>
                         <div id='choose-form'>
                             <NavLink to='/login' className='form-type'> SIGN IN </NavLink>
-
                             <NavLink to='/register' className='form-type'> SIGN UP </NavLink>
                         </div>
-
                         <h2>CREATE ACCOUNT</h2>
                         <div className='register-mode'>
                             <div className='register-mode-option'>
-                                <div className={registerTypeIsUser ? 'register-mode-icon-container-active' : 'register-mode-icon-container'}
-                                    onClick={changeRegisterTypeToUser}
+                                <div
+                                    className={formData.mode === FormDataMode.user ? 'register-mode-icon-container-active' : 'register-mode-icon-container'}
+                                    onClick={dispatch.bind(null, { type: ActionTypes.CHANGE_MODE, payload: FormDataMode.user })}
                                 >
                                     <PersonIcon className='register-mode-icon' />
                                     <h3>USER</h3>
                                 </div>
                             </div>
-
                             <div className='register-mode-option'>
-                                <div className={!registerTypeIsUser ? 'register-mode-icon-container-active' : 'register-mode-icon-container'}
-                                    onClick={changeRegisterTypeToAgency}
+                                <div
+                                    className={formData.mode === FormDataMode.agency ? 'register-mode-icon-container-active' : 'register-mode-icon-container'}
+                                    onClick={dispatch.bind(null, { type: ActionTypes.CHANGE_MODE, payload: FormDataMode.agency })}
                                 >
                                     <GroupsIcon className='register-mode-icon' />
                                     <h3>AGENCY</h3>
                                 </div>
                             </div>
                         </div>
-
-
                         <div id="register-email" className='inputField'>
                             <TextField
                                 fullWidth
                                 label="Email"
                                 variant="standard"
                                 color="secondary"
-                                error={false}
+                                helperText={formData.email.error && "Invalid Email"}
+                                error={formData.email.error}
+                                value={formData.email.value}
+                                onChange={(e) => { dispatch({ type: ActionTypes.CHANGE_EMAIL, payload: e.target.value }) }}
+                                onBlur={(e) => { dispatch({ type: ActionTypes.VALIDATE_EMAIL, payload: e.target.value }) }}
                             />
                         </div>
-
-
-                        {registerTypeIsUser
-
+                        {formData.mode === FormDataMode.user
                             ? <>
                                 <div id="register-firstName" className='inputField'>
                                     <TextField
@@ -91,21 +81,27 @@ const Register: FC = () => {
                                         label="First Name"
                                         variant="standard"
                                         color="secondary"
-                                        error={false}
+                                        helperText={formData.firstName.error && "First name should be at least 2 characters long!"}
+                                        error={formData.firstName.error}
+                                        value={formData.firstName.value}
+                                        onChange={(e) => { dispatch({ type: ActionTypes.CHANGE_FIRSTNAME, payload: e.target.value }) }}
+                                        onBlur={(e) => { dispatch({ type: ActionTypes.VALIDATE_FIRSTNAME, payload: e.target.value }) }}
                                     />
                                 </div>
-
                                 <div id="register-lastName" className='inputField'>
                                     <TextField
                                         fullWidth
                                         label="Last Name"
                                         variant="standard"
                                         color="secondary"
-                                        error={false}
+                                        helperText={formData.lastName.error && "Last name should be at least 2 characters long!"}
+                                        error={formData.lastName.error}
+                                        value={formData.lastName.value}
+                                        onChange={(e) => { dispatch({ type: ActionTypes.CHANGE_LASTNAME, payload: e.target.value }) }}
+                                        onBlur={(e) => { dispatch({ type: ActionTypes.VALIDATE_LASTNAME, payload: e.target.value }) }}
                                     />
                                 </div>
                             </>
-
                             : <>
                                 <div id="register-agencyName" className='inputField'>
                                     <TextField
@@ -113,7 +109,11 @@ const Register: FC = () => {
                                         label="Agency Name"
                                         variant="standard"
                                         color="secondary"
-                                        error={false}
+                                        helperText={formData.agencyName.error && "Agency name should be at least 2 characters long!"}
+                                        error={formData.agencyName.error}
+                                        value={formData.agencyName.value}
+                                        onChange={(e) => { dispatch({ type: ActionTypes.CHANGE_AGENCYNAME, payload: e.target.value }) }}
+                                        onBlur={(e) => { dispatch({ type: ActionTypes.VALIDATE_AGENCYNAME, payload: e.target.value }) }}
                                     />
                                 </div>
                                 <div id="register-agencyCity" className='inputField'>
@@ -122,28 +122,42 @@ const Register: FC = () => {
                                         label="City"
                                         variant="standard"
                                         color="secondary"
-                                        error={false}
+                                        helperText={formData.city.error && "City should be at least 3 characters long!"}
+                                        error={formData.city.error}
+                                        value={formData.city.value}
+                                        onChange={(e) => { dispatch({ type: ActionTypes.CHANGE_CITY, payload: e.target.value }) }}
+                                        onBlur={(e) => { dispatch({ type: ActionTypes.VALIDATE_CITY, payload: e.target.value }) }}
                                     />
                                 </div>
-                                <div id="register-agencyAdress" className='inputField'>
+                                <div id="register-agencyAddress" className='inputField'>
                                     <TextField
                                         fullWidth
-                                        label="Adress"
+                                        label="Address"
                                         variant="standard"
                                         color="secondary"
-                                        error={false}
+                                        helperText={formData.address.error && "Address should be at least 3 characters long!"}
+                                        error={formData.address.error}
+                                        value={formData.address.value}
+                                        onChange={(e) => { dispatch({ type: ActionTypes.CHANGE_ADDRESS, payload: e.target.value }) }}
+                                        onBlur={(e) => { dispatch({ type: ActionTypes.VALIDATE_ADDRESS, payload: e.target.value }) }}
                                     />
                                 </div>
                             </>
                         }
-
                         <div id="register-password" className='inputPassword'>
-                            <FormControl fullWidth variant="standard" color="secondary">
-                                <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-                                <Input
-                                    id="standard-adornment-password"
-                                    type={showPassword ? 'text' : 'password'}
-                                    endAdornment={
+                            <TextField
+                                fullWidth
+                                label="Password"
+                                variant="standard"
+                                color="secondary"
+                                helperText={formData.password.error && "Password should be at least 8 characters"}
+                                error={formData.password.error}
+                                value={formData.password.value}
+                                onChange={(e) => { dispatch({ type: ActionTypes.CHANGE_PASSWORD, payload: e.target.value }) }}
+                                onBlur={(e) => { dispatch({ type: ActionTypes.VALIDATE_PASSWORD, payload: e.target.value }) }}
+                                type={showPassword ? 'text' : 'password'}
+                                InputProps={{
+                                    endAdornment: (
                                         <InputAdornment position="end" className='password-visibilty-icon'>
                                             <IconButton
                                                 aria-label="toggle password visibility"
@@ -152,17 +166,24 @@ const Register: FC = () => {
                                                 {showPassword ? <VisibilityOff /> : <Visibility />}
                                             </IconButton>
                                         </InputAdornment>
-                                    }
-                                />
-                            </FormControl>
+                                    )
+                                }}
+                            />
                         </div>
                         <div id="register-rePassword" className='inputPassword'>
-                            <FormControl fullWidth variant="standard" color="secondary">
-                                <InputLabel htmlFor="standard-adornment-repeat-password">Repeat Password</InputLabel>
-                                <Input
-                                    id="standard-adornment-repeat-password"
-                                    type={showRePassword ? 'text' : 'password'}
-                                    endAdornment={
+                            <TextField
+                                fullWidth
+                                label="Repeat Password"
+                                variant="standard"
+                                color="secondary"
+                                helperText={formData.rePassword.error && "Passwords do not match"}
+                                error={formData.rePassword.error}
+                                value={formData.rePassword.value}
+                                onChange={(e) => { dispatch({ type: ActionTypes.CHANGE_REPASSWORD, payload: e.target.value }) }}
+                                onBlur={(e) => { dispatch({ type: ActionTypes.VALIDATE_REPASSWORD, payload: e.target.value }) }}
+                                type={showRePassword ? 'text' : 'password'}
+                                InputProps={{
+                                    endAdornment: (
                                         <InputAdornment position="end" className='password-visibilty-icon'>
                                             <IconButton
                                                 aria-label="toggle password visibility"
@@ -171,9 +192,9 @@ const Register: FC = () => {
                                                 {showRePassword ? <VisibilityOff /> : <Visibility />}
                                             </IconButton>
                                         </InputAdornment>
-                                    }
-                                />
-                            </FormControl>
+                                    )
+                                }}
+                            />
                         </div>
                         <div id='auth-button-container'>
                             <Button fullWidth variant="contained" size='large' id='auth-button' type='submit'>Register</Button>
---
+++ src/Pages/Auth/useRegisterFormData.ts
@@ -0,0 +1,144 @@
+import { Dispatch, useReducer } from "react"
+
+export enum FormDataMode {
+    'user' = 'user',
+    'agency' = 'agency'
+}
+
+type FormData = {
+    mode: FormDataMode
+    email: { error: boolean, value: string },
+    password: { error: boolean, value: string },
+    rePassword: { error: boolean, value: string }
+}
+
+type UserFormData = FormData & {
+    firstName: { error: boolean, value: string },
+    lastName: { error: boolean, value: string },
+}
+
+type AgencyFormData = FormData & {
+    agencyName: { error: boolean, value: string },
+    city: { error: boolean, value: string },
+    address: { error: boolean, value: string },
+}
+
+type Action = {
+    type: string,
+    payload: any
+}
+
+const initialState: UserFormData & AgencyFormData = {
+    mode: FormDataMode.user,
+    email: { error: false, value: '' },
+    firstName: { error: false, value: '' },
+    lastName: { error: false, value: '' },
+    agencyName: { error: false, value: '' },
+    city: { error: false, value: '' },
+    address: { error: false, value: '' },
+    password: { error: false, value: '' },
+    rePassword: { error: false, value: '' },
+}
+
+export enum ActionTypes {
+    'CHANGE_MODE' = 'CHANGE_MODE',
+    'CHANGE_EMAIL' = 'CHANGE_EMAIL',
+    'VALIDATE_EMAIL' = 'VALIDATE_EMAIL',
+    'CHANGE_PASSWORD' = 'CHANGE_PASSWORD',
+    'VALIDATE_PASSWORD' = 'VALIDATE_PASSWORD',
+    'CHANGE_REPASSWORD' = 'CHANGE_REPASSWORD',
+    'VALIDATE_REPASSWORD' = 'VALIDATE_REPASSWORD',
+    'CHANGE_FIRSTNAME' = 'CHANGE_FIRSTNAME',
+    'VALIDATE_FIRSTNAME' = 'VALIDATE_FIRSTNAME',
+    'CHANGE_LASTNAME' = 'CHANGE_LASTNAME',
+    'VALIDATE_LASTNAME' = 'VALIDATE_LASTNAME',
+    'CHANGE_AGENCYNAME' = 'CHANGE_AGENCYNAME',
+    'VALIDATE_AGENCYNAME' = 'VALIDATE_AGENCYNAME',
+    'CHANGE_CITY' = 'CHANGE_CITY',
+    'VALIDATE_CITY' = 'VALIDATE_CITY',
+    'CHANGE_ADDRESS' = 'CHANGE_ADDRESS',
+    'VALIDATE_ADDRESS' = 'VALIDATE_ADDRESS'
+}
+
+const formDataReducer = (state: UserFormData & AgencyFormData, action: Action) => {
+
+    switch (action.type) {
+        case ActionTypes.CHANGE_MODE:
+            return { ...state, mode: action.payload }
+        case ActionTypes.CHANGE_EMAIL:
+            return { ...state, email: { error: state.email.error, value: action.payload.trim() } }
+        case ActionTypes.VALIDATE_EMAIL:
+            if (action.payload.trim().length < 3 && action.payload.trim() !== '') {
+                return { ...state, email: { error: true, value: state.email.value } }
+            } else {
+                return { ...state, email: { error: false, value: state.email.value } }
+            }
+        case ActionTypes.CHANGE_PASSWORD:
+            return { ...state, password: { error: state.password.error, value: action.payload.trim() } }
+        case ActionTypes.VALIDATE_PASSWORD:
+            if (action.payload.trim().length < 8 && action.payload !== '') {
+                return { ...state, password: { error: true, value: state.password.value } }
+            } else {
+                return { ...state, password: { error: false, value: state.password.value } }
+            }
+        case ActionTypes.CHANGE_REPASSWORD:
+            return { ...state, rePassword: { error: state.rePassword.error, value: action.payload.trim() } }
+        case ActionTypes.VALIDATE_REPASSWORD:
+            if (action.payload !== state.password.value) {
+                return { ...state, rePassword: { error: true, value: state.rePassword.value } }
+            } else {
+                return { ...state, rePassword: { error: false, value: state.rePassword.value } }
+            }
+        case ActionTypes.CHANGE_FIRSTNAME:
+            return { ...state, firstName: { error: state.firstName.error, value: action.payload.trimStart() } }
+        case ActionTypes.VALIDATE_FIRSTNAME:
+            if (action.payload.trim().length < 2 && action.payload !== '') {
+                return { ...state, firstName: { error: true, value: state.firstName.value.trim() } }
+            } else {
+                return { ...state, firstName: { error: false, value: state.firstName.value.trim() } }
+            }
+        case ActionTypes.CHANGE_LASTNAME:
+            return { ...state, lastName: { error: state.lastName.error, value: action.payload.trimStart() } }
+        case ActionTypes.VALIDATE_LASTNAME:
+            if (action.payload.trim().length < 2 && action.payload !== '') {
+                return { ...state, lastName: { error: true, value: state.lastName.value.trim() } }
+            } else {
+                return { ...state, lastName: { error: false, value: state.lastName.value.trim() } }
+            }
+        case ActionTypes.CHANGE_AGENCYNAME:
+            return { ...state, agencyName: { error: state.agencyName.error, value: action.payload.trimStart() } }
+        case ActionTypes.VALIDATE_AGENCYNAME:
+            if (action.payload.trim().length < 2 && action.payload !== '') {
+                return { ...state, agencyName: { error: true, value: state.agencyName.value.trim() } }
+            } else {
+                return { ...state, agencyName: { error: false, value: state.agencyName.value.trim() } }
+            }
+        case ActionTypes.CHANGE_CITY:
+            return { ...state, city: { error: state.city.error, value: action.payload.trimStart() } }
+        case ActionTypes.VALIDATE_CITY:
+            if (action.payload.trim().length < 3 && action.payload !== '') {
+                return { ...state, city: { error: true, value: state.city.value.trim() } }
+            } else {
+                return { ...state, city: { error: false, value: state.city.value.trim() } }
+            }
+        case ActionTypes.CHANGE_ADDRESS:
+            return { ...state, address: { error: state.address.error, value: action.payload.trimStart() } }
+        case ActionTypes.VALIDATE_ADDRESS:
+            if (action.payload.trim().length < 3 && action.payload !== '') {
+                return { ...state, address: { error: true, value: state.address.value.trim() } }
+            } else {
+                return { ...state, address: { error: false, value: state.address.value.trim() } }
+            }
+        default:
+            return state
+    }
+
+}
+
+export const useRegisterFormData =
+    (state: UserFormData & AgencyFormData = initialState): [UserFormData & AgencyFormData, Dispatch<Action>] => {
+
+        const [data, dispatch] = useReducer(formDataReducer, initialState)
+
+        return [data, dispatch]
+    }

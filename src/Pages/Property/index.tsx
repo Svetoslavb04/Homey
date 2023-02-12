8c543428542676d6cc7de8917c7a864188e03e75
martinyovkov <martiyovkov10@gmail.com>
2023-02-12 02:27:56 +0200

    updatee property page

--- src/Pages/Property/index.tsx
+++ src/Pages/Property/index.tsx
@@ -1,4 +1,3 @@
-
 import './Property.scss'
 import CloseIcon from '@mui/icons-material/Close';
 import ArrowBack from '@mui/icons-material/ArrowBackIosRounded';
@@ -6,6 +5,13 @@ import ArrowForward from '@mui/icons-material/ArrowForwardIosRounded';
 import { FC, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import WifiIcon from '@mui/icons-material/Wifi';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import FireplaceIcon from '@mui/icons-material/Fireplace';
+import BalconyIcon from '@mui/icons-material/Balcony';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import PoolIcon from '@mui/icons-material/Pool';
+import LocalParkingIcon from '@mui/icons-material/LocalParking';
 
 
 const Property: FC = ()=> {
@@ -16,19 +22,21 @@ const Property: FC = ()=> {
     const fetchedProperty = {
         id: '3',
         name: 'Modern Villa',
-        numberOfRooms: 3,
+        status: 'for sale',
+        type: 'Villa',
+        price: '120 000',  
+        size: 200, 
+        numberOfBedrooms: 3,
         numberOfBathrooms: 1,
-        size: 200,
         description: "Company non lorem ac erat suscipit bibendum. Nulla facilisi. Sedeuter nunc volutpat, mollis sapien vel, conseyer turpeutionyer masin libero sempe. Fusceler mollis augue sit amet hendrerit vestibulum. Duisteyerionyer venenatis lacus.",
         year: '2021',
-        broker: 'John Wattson',
         agency: 'Era',
-        topImg: 'https://www.sunset.com/wp-content/uploads/medium_2x/hometour-Klopf-Architect-Sacramento-covered-porch-Mariko-Reed-0621.jpg',
-        primaryImg: 'https://www.mydomaine.com/thmb/dke2LC6lH21Pvqwd2lI6AIutnDY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/SuCasaDesign-Modern-9335be77ca0446c7883c5cf8d974e47c.jpg',
-        secondaryImg1: 'https://www.cuded.com/wp-content/uploads/2013/04/Caruth-Boulevard-Residence-by-Tom-Reisenbichler-1.jpg',
-        secondaryImg2: 'https://www.sunset.com/wp-content/uploads/medium_2x/hometour-Klopf-Architect-Sacramento-covered-porch-Mariko-Reed-0621.jpg'
+        images: ['https://www.sunset.com/wp-content/uploads/medium_2x/hometour-Klopf-Architect-Sacramento-covered-porch-Mariko-Reed-0621.jpg',
+                'https://www.mydomaine.com/thmb/dke2LC6lH21Pvqwd2lI6AIutnDY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/SuCasaDesign-Modern-9335be77ca0446c7883c5cf8d974e47c.jpg',
+                 'https://www.cuded.com/wp-content/uploads/2013/04/Caruth-Boulevard-Residence-by-Tom-Reisenbichler-1.jpg', 
+                 'https://www.sunset.com/wp-content/uploads/medium_2x/hometour-Klopf-Architect-Sacramento-covered-porch-Mariko-Reed-0621.jpg'],
+        extras: ['Wifi', 'Air Conditioning', 'Fire Place', 'Balcony', 'Fitness', 'Swimming Pool', 'Parking']
     }
-
 
     const [imageToOpen, SetImageToOpen] = useState('');
 
@@ -40,21 +48,62 @@ const Property: FC = ()=> {
         SetImageToOpen(oldValue => oldValue = '');
     }
     
+    const IconifyiedExtras = [ {
+        label: 'Wifi',
+        Icon: WifiIcon,
+        name: 'wifi'
+      }, {
+        label: 'Air Conditioning',
+        Icon: AcUnitIcon,
+        name: 'airConditioning'
+      }, {
+        label: 'Fire Place',
+        Icon: FireplaceIcon,
+        name: 'fireplace'
+      }, {
+        label: 'Balcony',
+        Icon: BalconyIcon,
+        name: 'balcony'
+      }, {
+        label: 'Fitness',
+        Icon: FitnessCenterIcon,
+        name: 'fitness'
+      }, {
+        label: 'Swimming Pool',
+        Icon: PoolIcon,
+        name: 'swimmingPool'
+      }, {
+        label: 'Parking',
+        Icon: LocalParkingIcon,
+        name: 'parking'
+      }];
+
+
+      const ext = [];
+    for (const extra of fetchedProperty.extras) {
+        const claim = IconifyiedExtras.find(c => c.label === extra)
+        if (claim) {
+            console.log(claim);
+            
+            ext.push(`${<claim.Icon/>} ${claim.label}`);
+        }
+        
+    }
+
     return (
     <div id='property-main'>
-         <div id='top-image' style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${fetchedProperty.topImg})` }}>
-            <div>
-                
+         <div id='top-image' style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${fetchedProperty.images[0]})` }}>
+            <div>  
                 <h1> Name of the property</h1>
             </div>
          </div>
 
          <div id='property-content'>
             <div id='property-images'>
-                <img id='property-primary-image' src={fetchedProperty.primaryImg} onClick={() => clickImageHandler(fetchedProperty.primaryImg)} />
+                <img id='property-primary-image' src={fetchedProperty.images[1]} onClick={() => clickImageHandler(fetchedProperty.images[1])} />
                 <div id='property-secondary-images'>
-                    <img src={fetchedProperty.secondaryImg1} onClick={() => clickImageHandler(fetchedProperty.secondaryImg1)} alt="house" />
-                    <img src={fetchedProperty.secondaryImg2} onClick={() => clickImageHandler(fetchedProperty.secondaryImg2)} alt="house" /> 
+                    <img src={fetchedProperty.images[2]} onClick={() => clickImageHandler(fetchedProperty.images[2])} alt="house" />
+                    <img src={fetchedProperty.images[3]} onClick={() => clickImageHandler(fetchedProperty.images[3])} alt="house" /> 
                 </div>
             </div>
             <div id='property-description'>
@@ -62,14 +111,16 @@ const Property: FC = ()=> {
                     mollis sapien vel, conseyer turpeutionyer masin libero sempe. Fusceler mollis augue 
                     sit amet hendrerit vestibulum. Duisteyerionyer venenatis lacus.
                 </p>
-                <p> Villa gravida eros ut turpis interdum ornare. Interdum et malesu they adamale fames ac
-                    anteipsu pimsine faucibus. Curabitur arcu site feugiat in torto.
-                </p>
+                <p><b className='property-info'>Status : </b> for sale</p>
+                <p><b className='property-info'>Type : </b> Villa</p>
                 <p><b className='property-info'>Price : </b> 120 000 €</p>
-                <p><b className='property-info'>Number of rooms : </b> 3 rooms</p>
-                <p><b className='property-info'>Number of bathrooms : </b> 1 bathroom</p>
                 <p><b className='property-info'>Site Size : </b> 120 m²</p>
+                <p><b className='property-info'>Number of bedrooms : </b> 3 rooms</p>
+                <p><b className='property-info'>Number of bathrooms : </b> 1 bathroom</p>                
                 <p><b className='property-info'>Year of construction : </b> July 2021</p> 
+                <p><b className='property-info'>Garages : </b> 1</p>
+                <hr></hr>
+                <p><b className='property-info'>Extras : </b> {ext} </p>
             </div>
 
          </div>

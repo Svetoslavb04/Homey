26c209f6e276a7b9f3febfa06e9adbe83dd770c8
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-02-13 21:30:43 +0200

    feat: add properties loader

--- src/Pages/Property/Listing/index.tsx
+++ src/Pages/Property/Listing/index.tsx
@@ -12,6 +12,7 @@ import Pagination from '@mui/material/Pagination';
 import { getFilteredData, getMetaData } from '../../../services/propertyService';
 import IPropertiesMeta from '../../../interfaces/IPropertiesMeta';
 import { homeyAPI } from '../../../assets/js/APIs';
+import LocalLoader from '../../../Components/LocalLoader';
 
 const defaultPageSize = 6;
 
@@ -19,10 +20,14 @@ const PropertiesListings = () => {
 
     const [filter, setFilter] = useState<IPropertyFilter>({});
     const [page, setPage] = useState<number>(1);
-    const [properties, setProperties] = useState<IProperty[]>([]);
     const [meta, setMeta] = useState<IPropertiesMeta>();
 
+    const [propertiesLoading, setPropertiesLoading] = useState<boolean>(true);
+    const [properties, setProperties] = useState<IProperty[]>([]);
+
     useEffect(() => {
+
+        setPropertiesLoading(true);
 
         getFilteredData({ ...filter, page, pageSize: defaultPageSize })
             .then(payload => {
@@ -41,19 +46,26 @@ const PropertiesListings = () => {
 
     }, [filter, page])
 
+    useEffect(() => {
+        if(propertiesLoading) { setPropertiesLoading(false)}
+    }, [properties, propertiesLoading])
+
     return (
         <div className='property-listing-container'>
             {!meta}
             <FilterSection metaData={meta} setFilter={setFilter} />
-            <PropertiesSection properties={properties} />
-            <section className='property-listing-pagination df jcc'>
-                <Pagination
-                    size={window.innerWidth < 680 ? 'small' : 'medium'}
-                    count={meta?.pages || 1} //Get from server
-                    page={page}
-                    onChange={(_, page) => setPage(page)} color='secondary'
-                />
-            </section>
+            <div style={{ position: 'relative', minHeight: '150px' }}>
+                {propertiesLoading && <LocalLoader />}
+                <PropertiesSection properties={properties} />
+                <section className='property-listing-pagination df jcc'>
+                    <Pagination
+                        size={window.innerWidth < 680 ? 'small' : 'medium'}
+                        count={meta?.pages || 1} //Get from server
+                        page={page}
+                        onChange={(_, page) => setPage(page)} color='secondary'
+                    />
+                </section>
+            </div>
         </div>
     )
 }

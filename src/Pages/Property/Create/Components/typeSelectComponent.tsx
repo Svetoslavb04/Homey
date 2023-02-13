badf122f5d7627a3bb9d9d2271db6ea9888b6b63
martinyovkov <martiyovkov10@gmail.com>
2023-02-13 12:50:10 +0200

    update create page

---
+++ src/Pages/Property/Create/Components/typeSelectComponent.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { MenuItem } from '@mui/material';
+
+export default function NativeSelectDemo() {
+  return (
+    <Box sx={{ minWidth: 180 }}>
+      <FormControl fullWidth variant="standard">
+        <InputLabel variant="standard" htmlFor="uncontrolled-native">
+          Type
+        </InputLabel>
+        <Select
+          name='type'
+          defaultValue="House"
+          label="Type"
+        >
+          <MenuItem value={'house'}>House</MenuItem>
+          <MenuItem value={'apartment'}>Apartment</MenuItem>
+          <MenuItem value={'villa'}>Villa</MenuItem>
+          <MenuItem value={'landfield'}>Landfield</MenuItem>
+          <MenuItem value={'other'}>Other</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
--- src/Pages/Property/Create/index.tsx
+++ src/Pages/Property/Create/index.tsx
@@ -1,40 +1,60 @@
 import './Create.scss';
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { NavLink } from "react-router-dom";
 
-import PersonIcon from '@mui/icons-material/Person';
-import GroupsIcon from '@mui/icons-material/Groups';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-
 import TextField from '@mui/material/TextField';
-import IconButton from '@mui/material/IconButton';
-import Input from '@mui/material/Input';
 import Button from '@mui/material/Button';
 import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
-import FormControl from '@mui/material/FormControl';
+import { countries } from '../../../assets/js/countries';
+import { citiesOfCountryURL } from '../../../assets/js/APIs';
+
+
+import Status from './Components/radioStatusComponent';
+import Type from './Components/typeSelectComponent';
+import Bedrooms from './Components/bedroomsSelectComponent';
+import Bathrooms from './Components/bathroomSelectComponent';
+import CountrySelect from '../../../Components/Core/CountrySelect';
+import { Autocomplete, SelectChangeEvent } from '@mui/material';
+
 
 
 
 
 const AddProperty: FC = () => {
+    
+    const [selectedCountry, setSelectedCountry] = useState<string>('Bulgaria');
+    const [availableCities, setAvailableCities] = useState<string[]>([]);
 
-    const [showPassword, setShowPassword] = useState(false);
-    const [showRePassword, setShowRePassword] = useState(false);
+    useEffect(() => {
 
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
-    const handleClickShowRePassword = () => setShowRePassword((show) => !show);
+        fetch('http://ip-api.com/json')
+          .then(res => res.json())
+          .then(payload => { setSelectedCountry(payload.countryCode) })
+    
+      }, [setSelectedCountry]);
 
-    const [registerTypeIsUser, setUserType] = useState(true);
 
-    const changeRegisterTypeToAgency = () => {
-        setUserType(false);
-    }
+      useEffect(() => {
 
-    const changeRegisterTypeToUser = () => {
-        setUserType(true);
-    }
+        if (!countries.some(c => c.code === selectedCountry)) { return setAvailableCities([]) }
+    
+        fetch(citiesOfCountryURL, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ country: countries.filter(c => c.code === selectedCountry)[0].name })
+        })
+          .then(res => res.json())
+          .then(payload => setAvailableCities(payload.data))
+          .catch(err => console.log(err))
+    
+      }, [selectedCountry]);
+    
+      const handleCountryChange = (e: SelectChangeEvent<string>) => setSelectedCountry(e.target.value);
+
+
 
 
     return (
@@ -43,10 +63,24 @@ const AddProperty: FC = () => {
             <div id='create-container'>
                 <div className='add-form-wrapper'>
                     <form id='add-form'>
-                        <h2>List property</h2>  
+                        <h1>List a property</h1>  
                         <div id='info-container'>
 
                             <div id='left-side'>
+                                <div id='country' className='inputField'>
+                                    <CountrySelect
+                                        className='property-filter-country-select'
+                                        label='Country'
+                                        value={selectedCountry}
+                                        onChange={handleCountryChange}
+                                    />
+                                </div>
+                                
+
+                                 <div id='type' className='inputField'> 
+                                    <Type/>
+                                 </div>       
+                                    
                                 <div id="name" className='inputField'>
                                     <TextField
                                         fullWidth
@@ -63,16 +97,13 @@ const AddProperty: FC = () => {
                                         variant="standard"
                                         color="secondary"
                                         error={false}
+                                        InputProps={{
+                                           endAdornment: <InputAdornment position="end">m²</InputAdornment>,
+                                          }}
                                     />
                                 </div>
-                                <div id="number-bedrooms" className='inputField'>
-                                    <TextField
-                                        fullWidth
-                                        label="Number of bedrooms"
-                                        variant="standard"
-                                        color="secondary"
-                                        error={false}
-                                    />
+                                <div id="number-batrooms" className='inputField'>
+                                    <Bathrooms/>
                                 </div>
                                 <div id="agency" className='inputField'>
                                     <TextField
@@ -85,7 +116,32 @@ const AddProperty: FC = () => {
                                 </div>
                             </div>
 
+
                             <div id='right-size'>
+                                
+                                <div id='city' className='inputField'>
+                                    <Autocomplete
+                                        freeSolo
+                                        clearOnBlur={false}
+                                        disablePortal
+                                        options={availableCities}
+                                        defaultValue=""
+                                        renderInput={(params) => (
+                                            <TextField
+                                                {...params}
+                                                label="City"
+                                                name='city'
+                                                placeholder='Enter a city'
+                                                className='property-filter-advanced-value'
+                                                variant='standard'
+                                            />
+                                        )}
+                                    />
+                                </div>
+
+                                <div id='status' className='inputField'> 
+                                    <Status/>
+                                </div>
                                 <div id="price" className='inputField'>
                                     <TextField
                                         fullWidth
@@ -95,14 +151,8 @@ const AddProperty: FC = () => {
                                         error={false}
                                     />
                                 </div> 
-                                <div id="number-bathrooms" className='inputField'>
-                                    <TextField
-                                        fullWidth
-                                        label="Number of bathrooms"
-                                        variant="standard"
-                                        color="secondary"
-                                        error={false}
-                                    />
+                                <div id="number-bedrooms" className='inputField'>
+                                    <Bedrooms/>
                                 </div>
                                 <div id="contact-number" className='inputField'>
                                     <TextField
@@ -118,7 +168,7 @@ const AddProperty: FC = () => {
                         
 
                         <div id='add-button-container'>
-                            <Button fullWidth variant="contained" size='large' id='add-button' type='submit'>Add</Button>
+                            <Button fullWidth variant="contained" size='large' id='add-button' type='submit'>Add Property</Button>
                         </div>
                     </form>
                 </div>
